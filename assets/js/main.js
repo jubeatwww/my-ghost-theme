818ec3d2357a6159e4db0a7c1c51fe9eb44bc5c0
Justin <l6104400@gmail.com>
2025-06-13 18:54:26 +0800

    build header and featured posts blocks

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,46 +18,19 @@ function darkMode() {
 }
 
 function carousel() {
-    var carousel = $('.carousel');
-    var postImage = carousel.find('.post-image');
-    var imageHeight, nav;
-
-    function moveNav() {
-        imageHeight = postImage.height();
-        if (!nav) {
-            nav = carousel.find('.owl-prev, .owl-next');
+    var splide = $('.splide');
+    new Splide(splide[0], {
+        type: 'loop',
+        speed: 800,
+        drag: 'free',
+        autoWidth: true,
+        arrows: false,
+        pagination: false,
+        breakpoints: {
+            769: {
+                focus: "center",
+                drag: !0
+            }
         }
-        nav.css({
-            top: imageHeight / 2 + 'px',
-            opacity: 1,
-        });
-    }
-
-    carousel.owlCarousel({
-        dots: false,
-        margin: 28,
-        nav: true,
-        navText: [
-            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="22" height="22" fill="currentColor"><path d="M20.547 22.107l-6.107-6.107 6.107-6.12-1.88-1.88-8 8 8 8 1.88-1.893z"></path></svg>',
-            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="22" height="22" fill="currentColor"><path d="M11.453 22.107l6.107-6.107-6.107-6.12 1.88-1.88 8 8-8 8-1.88-1.893z"></path></svg>',
-        ],
-        onInitialized: function () {
-            moveNav();
-            carousel.css('visibility', 'visible');
-        },
-        onResized: function () {
-            moveNav();
-        },
-        responsive: {
-            0: {
-                items: 1,
-            },
-            768: {
-                items: 3,
-            },
-            992: {
-                items: 4,
-            },
-        },
-    });
+    }).mount();
 }
